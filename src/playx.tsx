06b8a1809a99b47a19b2436da733bcccb0ac28da
Mutasim Issa <mutasim@sourcya.com>
2020-08-01 23:28:53 +0200

    Responsive fixes, added button for editor in the header

--- src/playx.tsx
+++ src/playx.tsx
@@ -1,172 +1,168 @@
 import React, { Component } from 'react';
 import { Stage, Layer, Image, Text } from 'react-konva';
-import { IonRange, IonButton, IonSelect, IonSelectOption, IonCard, IonHeader, IonTitle, IonToolbar, IonInput, IonItem, IonLabel } from '@ionic/react';
-
-
-export class PlayHeader extends Component {
-  render() {
-    return (
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>PlayX Demo</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-    );
-  }
-}
+import { IonRange, IonIcon, IonSelect, IonSelectOption, IonCard, IonHeader, IonTitle, IonToolbar, IonInput, IonItem, IonLabel, IonButtons, IonButton } from '@ionic/react';
+import { createOutline } from 'ionicons/icons';
 
 export class Playx extends Component {
-  
-  bg = new window.Image(window.innerWidth,window.innerHeight)
+
+  bg = new window.Image(window.innerWidth, window.innerHeight)
   state = {
-    newTextObj: {
-      textEditVisible: false,
-      textX: 0,
-      textY: 0,
-      textValue: "Hello",
-      fontSize: 8,
-      width: window.innerWidth,
-      fontStyle: "normal",
-      align: "left",
-      id: 0,
-      sizeValue: 96,
-      colorValue: "black"
-    },
+    textTitle: {
+    textEditVisible: false,
+    textX: 0,
+    textY: 0,
+    textValue: "Hello",
+    width: window.innerWidth,
+    fontStyle: "normal",
+    align: "center",
+    id: 0,
+    sizeValue: 96,
+    colorValue: "black"
+  },
     bg: {
       src: 'https://sacrebleu-galerie.com/wp-content/uploads/2018/06/clean-white-brick-wall-textures-plain.jpg'
     }
   };
-  
+
   handleTextareaKeyDown = (e: any) => {
     if (e.keyCode === 13) {
-      let { newTextObj } = this.state;
+          let {textTitle} = this.state;
 
-      newTextObj.textEditVisible = false;
+      textTitle.textEditVisible = false;
       this.setState({
-        newTextObj
-      });
+          textTitle
+        });
     }
   };
 
   handleCloseButton = () => {
-    let { newTextObj } = this.state;
-    newTextObj.textEditVisible = false;
+          let {textTitle} = this.state;
+    textTitle.textEditVisible = false;
     this.setState({
-      newTextObj
-    });
+          textTitle
+        });
   };
 
   handleTextEdit = (e: any) => {
-    let { newTextObj } = this.state;
-    newTextObj.textValue = e.target.value;
+          let {textTitle} = this.state;
+    textTitle.textValue = e.target.value;
     this.setState({
-      newTextObj
-    });
+          textTitle
+        });
   };
 
   handleSizeEdit = (e: any) => {
-    let { newTextObj } = this.state;
-    newTextObj.sizeValue = e.target.value;
+          let {textTitle} = this.state;
+    textTitle.sizeValue = e.target.value;
     this.setState({
-      newTextObj
-    });
+          textTitle
+        });
   };
 
   handleColorEdit = (e: any) => {
-    let { newTextObj } = this.state;
-    newTextObj.colorValue = e.target.value;
+          let {textTitle} = this.state;
+    textTitle.colorValue = e.target.value;
     this.setState({
-      newTextObj
-    });
+          textTitle
+        });
   };
-  handleTextDblClick = (e: any) => {
-    const absPos = e.target.getAbsolutePosition();
-    let { newTextObj } = this.state;
-    newTextObj.textEditVisible = true;
-    newTextObj.textX = absPos.x;
-    newTextObj.textY = absPos.y;
+  handleTextClick = (e: any) => {
+    let absPos = {x: 0 , y: 0}
+    let { textTitle} = this.state;
+    if (!textTitle.textEditVisible) {
+      textTitle.textEditVisible = true;
+    } else {
+      textTitle.textEditVisible = false;
+    }
+    textTitle.textX = absPos.x;
+    textTitle.textY = absPos.y;
     this.setState({
-      newTextObj
-    });
+          textTitle
+        });
   };
   render() {
-    this.bg.src = this.state.bg.src;
+          this.bg.src = this.state.bg.src;
     return (
-      <div>
-        <PlayHeader />
-        <IonCard
-          style={{
-            display: this.state.newTextObj.textEditVisible ? "block" : "none",
-            position: "absolute",
-            // top: this.state.newTextObj.textY + "px",
-            // left: this.state.newTextObj.textX + "px"
-            bottom: "5vh",
+        <div>
+          <IonHeader>
+        <IonToolbar>
+          <IonTitle>PlayX Demo</IonTitle>
+          <IonButtons slot="primary">
+            <IonButton onClick={(e)=> {this.handleTextClick(e)}} color="danger">
+              <IonIcon slot="end" icon={createOutline} />
+            </IonButton>
+          </IonButtons>
+        </IonToolbar>
+      </IonHeader>
 
-          }}
-        >
-          <IonItem>
-            <IonLabel position="fixed">Title</IonLabel>
-            <IonInput
-              value={this.state.newTextObj.textValue}
-              onIonChange={e => this.handleTextEdit(e)}
-              onKeyDown={e => this.handleTextareaKeyDown(e)}
-              placeholder="Title"
-            >
-            </IonInput>
-          </IonItem>
-          <IonItem>
-            <IonLabel position="fixed">Font Size</IonLabel>
-            <IonRange
-              value={this.state.newTextObj.sizeValue}
-              min={64}
-              max={250}
-              step={2}
-              pin={true}
-              snaps={true}
-              onIonChange={e => this.handleSizeEdit(e)}
-            />
-          </IonItem>
-          <IonItem>
-            <IonLabel position="fixed">Color</IonLabel>
-            <IonSelect
-              value={this.state.newTextObj.colorValue}
-              onIonChange={e => this.handleColorEdit(e)}>
-              <IonSelectOption value="black">Black</IonSelectOption>
-              <IonSelectOption value="green">Green</IonSelectOption>
-              <IonSelectOption value="red">Red</IonSelectOption>
-              <IonSelectOption value="blue">Blue</IonSelectOption>
-            </IonSelect>
-          </IonItem>
-          <IonItem>
-            <IonButton
-              color="danger"
-              expand="block"
-              onClick={() => this.handleCloseButton()}
-            >
-              Close
+          <IonCard
+            style={{
+              display: this.state.textTitle.textEditVisible ? "block" : "none",
+              position: "absolute",
+              bottom: "5vh",
+
+            }}
+          >
+            <IonItem>
+              <IonLabel position="fixed">Title</IonLabel>
+              <IonInput
+                value={this.state.textTitle.textValue}
+                onIonChange={e => this.handleTextEdit(e)}
+                onKeyDown={e => this.handleTextareaKeyDown(e)}
+                placeholder="Title"
+              >
+              </IonInput>
+            </IonItem>
+            <IonItem>
+              <IonLabel position="fixed">Font Size</IonLabel>
+              <IonRange
+                value={this.state.textTitle.sizeValue}
+                min={64}
+                max={250}
+                step={2}
+                pin={true}
+                snaps={true}
+                onIonChange={e => this.handleSizeEdit(e)}
+              />
+            </IonItem>
+            <IonItem>
+              <IonLabel position="fixed">Color</IonLabel>
+              <IonSelect
+                value={this.state.textTitle.colorValue}
+                onIonChange={e => this.handleColorEdit(e)}>
+                <IonSelectOption value="black">Black</IonSelectOption>
+                <IonSelectOption value="green">Green</IonSelectOption>
+                <IonSelectOption value="red">Red</IonSelectOption>
+                <IonSelectOption value="blue">Blue</IonSelectOption>
+              </IonSelect>
+            </IonItem>
+            <IonItem>
+              <IonButton
+                color="danger"
+                expand="block"
+                onClick={() => this.handleCloseButton()}
+              >
+                Close
           </IonButton>
-          </IonItem>
-        </IonCard>
-        <Stage width={window.innerWidth} height={window.innerHeight}>
-          <Layer>
-            <Image image={this.bg}/>
-            <Text
-              fontSize={this.state.newTextObj.sizeValue}
-              draggable
-              text={this.state.newTextObj.textValue}
-              x={100}
-              y={100}
-              wrap="word"
-              width={this.state.newTextObj.width}
-              fill={this.state.newTextObj.colorValue}
-              onClick={e => this.handleTextDblClick(e)}
-            />
-          </Layer>
-        </Stage>
-      </div>
+            </IonItem>
+          </IonCard>
+          <Stage width={window.innerWidth} height={window.innerHeight}>
+            <Layer>
+              <Image image={this.bg} />
+              <Text
+                fontSize={this.state.textTitle.sizeValue}
+                draggable
+                text={this.state.textTitle.textValue}
+                x={100}
+                y={100}
+                wrap="word"
+                width={this.state.textTitle.width}
+                fill={this.state.textTitle.colorValue}
+                onClick={e => this.handleTextClick(e)}
+              />
+            </Layer>
+          </Stage>
+        </div>
     );
   }
 }
-
-
-// export default ColoredRect;
